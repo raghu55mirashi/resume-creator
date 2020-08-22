e8b230d3ad58d9dc891ece7d44d88a68e5a6a3bc
raghu55mirashi <raghu55mirashi@gmail.com>
2020-08-23 00:52:25 +0530

    hover effect added

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = ({ onclick, label }) => {
     return (
-        <h2 className="capitalize p-1 text-base text-black text-center cursor-pointer border-solid border-white border-t-2"
+        <h2 className="hover:bg-gray-200 capitalize p-1 text-base text-black text-center cursor-pointer border-solid border-white border-t-2"
             onClick={onclick}>{label}</h2>
     )
 }
